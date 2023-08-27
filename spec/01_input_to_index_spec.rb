7bff6333a63484861ad0d02eae2cb319bf1a6162
Noah A Klem <noahaklem@gmail.com>
2023-08-26 19:51:39 -0600

    input_to_index method defined

--- spec/01_input_to_index_spec.rb
+++ spec/01_input_to_index_spec.rb
@@ -1,26 +1,53 @@
 require_relative "../lib/move.rb"
 
-describe '#input_to_index' do
+describe "#input_to_index" do
+  it "defines a input_to_index method" do 
+    expect(defined?(input_to_index)).to be_truthy
+  end
 
-  it 'converts a user_input to an integer' do
+  it "converts user_input to an integer" do 
     user_input = "1"
     converted_input = input_to_index(user_input)
-    
+
     expect(converted_input).to be_a(Integer)
   end
 
-  it 'subtracts 1 from the user_input' do
-    user_input = "6"
+  it "subtracts 1 from the user_input" do
+    user_input = 5
     converted_input = input_to_index(user_input)
 
-    expect(converted_input).to be(5)
+    expect(converted_input).to be(4)
   end
 
-  it 'returns -1 for strings without integers' do
+  it "returns -1 for strings without integers" do 
     user_input = "invalid"
     converted_input = input_to_index(user_input)
-    
+
     expect(converted_input).to be(-1)
   end
-
 end
+
+# describe '#input_to_index' do
+
+#   it 'converts a user_input to an integer' do
+#     user_input = "1"
+#     converted_input = input_to_index(user_input)
+    
+#     expect(converted_input).to be_a(Integer)
+#   end
+
+#   it 'subtracts 1 from the user_input' do
+#     user_input = "6"
+#     converted_input = input_to_index(user_input)
+
+#     expect(converted_input).to be(5)
+#   end
+
+#   it 'returns -1 for strings without integers' do
+#     user_input = "invalid"
+#     converted_input = input_to_index(user_input)
+    
+#     expect(converted_input).to be(-1)
+#   end
+
+# end
